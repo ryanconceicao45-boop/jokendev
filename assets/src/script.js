a60054cdaa866237673d52b03cfe87475e8cc292
Ryan <257421077+ryanconceicao45-boop@users.noreply.github.com>
2026-05-29 01:38:36 -0300

    Versao 0.9

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,35 +1,37 @@
-// const barra = document.querySelector('.load-barra')
-// const LoadTela = document.querySelector('.Load-tela')
-// const HomeTela = document.querySelector('.tela-home')
-// const textInfos = document.querySelector('.text-infos')
+const barra = document.querySelector('.load-barra')
+const LoadTela = document.querySelector('.Load-tela')
+const HomeTela = document.querySelector('.tela-home')
+const textInfos = document.querySelector('.text-infos')
 
-// let dados = await fetch('./assets/src/config.json').then(r => r.json())
+let dados = await fetch('./assets/src/config.json').then(r => r.json())
 
-// const devSom = (som) => {
-//     if (som === 'loadsom') {
-//         const somLoad = new Audio('./assets/sound/inicio.mp3')
-//         somLoad.play()
-//     }
-// }
+const devSom = (som) => {
+    if (som === 'loadsom') {
+        const somLoad = new Audio('./assets/sound/inicio.mp3')
+        somLoad.play()
+    }
+}
 
-// const mainInfo = () => {
-//     LoadTela.style.display = `none`
-//     HomeTela.style.display = `flex`
-//     textInfos.innerHTML = dados.Main.TextInfo
-//     devSom('loadsom')
-// }
+const mainInfo = () => {
+    LoadTela.style.display = `none`
+    HomeTela.style.display = `flex`
+    textInfos.innerHTML = dados.Main.TextInfo
+    devSom('loadsom')
+}
 
-// function loadScreen() {
-//     barra.style.animation = `loadbar ${dados.Main.timeScrenn}s linear`
-//     setTimeout(() => {
-//         LoadTela.style.opacity = `0`
-//         setTimeout(mainInfo, 3000)
-//     }, dados.Main.timeScrenn * 1000);
-// }
-// loadScreen()
+function loadScreen() {
+    barra.style.animation = `loadbar ${dados.Main.timeScrenn}s linear`
+    setTimeout(() => {
+        LoadTela.style.opacity = `0`
+        setTimeout(mainInfo, 3000)
+    }, dados.Main.timeScrenn * 1000);
+}
+loadScreen()
 
-// const bntIniciar = document.querySelector('.btn-iniciar')
+const bntIniciar = document.querySelector('.btn-iniciar')
+const mainPartida = document.querySelector('.bg-main')
 
-// bntIniciar.addEventListener('click', function() {
-//     console.log('Click')
-// })
+bntIniciar.addEventListener('click', function () {
+    HomeTela.style.display = `none`
+    mainPartida.style.display = `block`
+})
